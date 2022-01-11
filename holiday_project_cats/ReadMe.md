5a9699bfdbb693907451dc55b70b86fe1f2b45a4
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-01-11 15:13:16 +0000

    Add files via upload

---
+++ holiday_project_cats/ReadMe.md
@@ -0,0 +1,6 @@
+Main Objective is to Create a Cat 2022 Calendar whereby user can 
+    - can change cat image by pressing a button 
+    - add a "to do" into a list which is save by date
+    - go to a particular date and retrive the to do list
+
+    
---
+++ holiday_project_cats/main.js
@@ -0,0 +1,76 @@
+
+// go to cat url to fetch cat image
+async function getCat() {
+    const response = await fetch ("https://cataas.com/cat/says/hello%20world!?cachebuster="+Math.random());
+    console.log(response);
+    const blob = await response.blob();
+    const imageObjectURL = URL.createObjectURL(blob);
+      console.log(imageObjectURL);
+
+
+//attach the image into the image with the id of cat-image    
+    let image = document.querySelector("#cat-image");
+    image.src = imageObjectURL;
+}
+getCat();
+
+//add even listener so that when user click, new image is display
+let button = document.querySelector("#new-image");
+button.addEventListener("click", getCat);
+
+
+// Create a "close" button and append it to each list item
+var myList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myList[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
